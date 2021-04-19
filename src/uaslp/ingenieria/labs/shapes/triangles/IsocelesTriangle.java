5869b5e935b1f7ba88b737020f66d577565d0116
LuisLC-Dev <lopezluis2c@gmail.com>
2021-04-19 05:22:41 -0500

    implementacion de triángulos escalenos e isóceles

---
+++ src/uaslp/ingenieria/labs/shapes/triangles/IsocelesTriangle.java
@@ -0,0 +1,15 @@
+package uaslp.ingenieria.labs.shapes.triangles;
+
+import uaslp.ingenieria.labs.shapes.Triangle;
+
+public class IsocelesTriangle extends Triangle {
+    private int side1;
+    private int sides23;
+
+    public IsocelesTriangle(int side1, int sides23) {
+        this.side1 = side1;
+        this.sides23 = sides23;
+    }
+
+
+}
---
+++ src/uaslp/ingenieria/labs/shapes/triangles/ScaleneTriangle.java
@@ -0,0 +1,15 @@
+package uaslp.ingenieria.labs.shapes.triangles;
+
+import uaslp.ingenieria.labs.shapes.Triangle;
+
+    public class ScaleneTriangle extends Triangle {
+        private int side1;
+        private int side2;
+        private int side3;
+
+        public ScaleneTriangle(int side1, int side2, int side3) {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+    }
